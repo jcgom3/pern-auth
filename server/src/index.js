2b55fbbe5326143bb89e10bc7692b2fe2e6b1676
Juan  Gomez <jcgom3@gmail.com>
2022-12-14 01:12:39 -0500

    commented some fx on server/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,22 +7,22 @@ const passport = require("passport");
 const cors = require("cors");
 
 // redirected client side to run on server
-const path = require("path");
-const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../client/build/");
+// const path = require("path");
+// const _dirname = path.dirname("");
+// const buildPath = path.join(_dirname, "../client/build/");
 
-app.use(express.static(buildPath));
+// app.use(express.static(buildPath));
 
-app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(
+//     path.join(__dirname, "../client/build/index.html"),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+// });
 
 //
 
